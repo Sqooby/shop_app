805ed8628ab5cce58d21cdaf99e346b4b399ff84
Sqooby <michal.basznianin3@gmail.com>
2022-10-16 11:23:45 +0200

    Product_Item_ditels

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -12,22 +14,13 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.black,
-              width: 4,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue,
-                spreadRadius: 7,
-                blurRadius: 10,
-                // changes position of shadow
-              ),
-            ],
-          ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
           child: Image.network(
             imageUrl,
             fit: BoxFit.cover,
