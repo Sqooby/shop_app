0029e6d0829f49d8f1a22a3fae906791b4653a19
Sqooby <michal.basznianin3@gmail.com>
2022-10-31 09:02:44 +0100

    cart

---
+++ lib/providers/cart.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final double price;
+  final String title;
+  final int quantity;
+
+  CartItem({
+    required this.id,
+    required this.price,
+    required this.quantity,
+    required this.title,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCardItem) => CartItem(
+              id: existingCardItem.id,
+              price: existingCardItem.price,
+              quantity: existingCardItem.quantity + 1,
+              title: existingCardItem.title));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          price: price,
+          quantity: 1,
+          title: title,
+        ),
+      );
+    }
+  }
+}
