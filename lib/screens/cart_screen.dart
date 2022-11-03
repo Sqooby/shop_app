3fb9a18819037ef1886aef2b65b7ba4ef7fee932
Sqooby <michal.basznianin3@gmail.com>
2022-11-03 17:53:27 +0100

    deletingCartItem

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -11,47 +12,63 @@ class CartScreen extends StatelessWidget {
     final cart = Provider.of<Cart>(context);
     String totalValue = cart.TotalValue.toString();
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Your Cart"),
-        ),
-        body: Column(
-          children: [
-            Card(
-              margin: EdgeInsets.all(10),
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Text(
-                      "Total",
-                      style: TextStyle(fontSize: 24),
+      appBar: AppBar(
+        title: Text("Your Cart"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(10),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text(
+                    "Total",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    label: Text(
+                      '\$${totalValue}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .headline6
+                              ?.color),
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Chip(
-                      label: Text(
-                        '\$${totalValue}',
-                        style: TextStyle(
-                            color: Theme.of(context)
-                                .primaryTextTheme
-                                .headline6
-                                ?.color),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  Spacer(),
+                  TextButton(
+                      child: Text(
+                        "Order Now!",
+                        style: TextStyle(fontSize: 20),
                       ),
-                      backgroundColor: Theme.of(context).primaryColor,
-                    ),
-                    Spacer(),
-                    TextButton(
-                        child: Text(
-                          "Order Now!",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        onPressed: () {})
-                  ],
-                ),
+                      onPressed: () {})
+                ],
               ),
             ),
-          ],
-        ));
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (ctx, i) => CartItem(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
+                cart.items.keys.toList()[i],
+              ),
+              itemCount: cart.ItemCount,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
